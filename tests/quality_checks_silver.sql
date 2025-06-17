0df91a1a60c65be02e26bb15348cc772b2e42d18
Santhosh S <231401094@rajalakshmi.edu.in>
2025-06-17 14:54:46 +0530

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -30,3 +30,21 @@ WHERE prd_cost<0 OR prd_cost IS NULL
 SELECT *
 FROM silver.crm_prd_info
 WHERE prd_end_dt<prd_start_dt
+
+
+--check for unwanted spaces
+SELECT * FROM bronze.erp_px_cat_g1v2
+WHERE cat!=TRIM(cat) OR subcat!=TRIM(subcat) OR maintenance!=TRIM(maintenance)
+
+--Data Standardization & Consistency
+SELECT DISTINCT(cat) --maintenance check too
+FROM bronze.erp_px_cat_g1v2
+
+SELECT * FROM silver.erp_px_cat_g1v2
+
+--Data Standardization & Consistency
+SELECT DISTINCT(cntry)
+FROM bronze.erp_loc_a101
+ORDER BY cntry
+
+SELECT * FROM silver.erp_loc_a101
